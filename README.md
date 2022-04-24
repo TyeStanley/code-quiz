ea8cacfd07c9293ad3ce6f309e0cdbea4f276822
Tye Stanley <tyestanley090@gmail.com>
2022-04-24 18:04:56 -0500

    add html foundation and js function

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# code-quiz
+# Code Quiz
+
+## Description
+
+This is an app that quizes you over JavaScript questions where you are timed to complete it. It also keeps a high score record where you can compete with your highest score. Can you get the perfect score? Try it!
+
+## Website Links
+
+[Live Website](https://tyestanley.github.io/code-quiz/)
+
+[Repository](https://github.com/TyeStanley/code-quiz)
+
+## Languages Used
+
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+
+## Example
+Please take a look at how the start screen looks:
+
+![Start of the Quiz Challenge](/assets/images/start-quiz.PNG)
+
+
+## License
+
+ MIT License
+
+Copyright (c) 2022 Tye Stanley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ assets/js/script.js
@@ -0,0 +1,276 @@
+// References for HTML
+// HighScore and Timer - links to top of the screen
+let viewHighScore = document.getElementById('view-high-score');
+let countdownTimer = document.getElementById('countdown-timer');
+let timeLeft = document.getElementById('time-left');
+let timesUp = document.getElementById('times-up');
+
+// Start Section - links to start section and the "Start Quiz" button
+let startSection = document.getElementById('start-section');
+let startBtn = document.getElementById('start-button');
+
+// Quiz Section - links to the quiz and buttons
+let quizSection = document.getElementById('quiz-section');
+let quizQuestion = document.getElementById('quiz-question');
+// Links to button elements
+let choiceA = document.getElementById('btn-0');
+let choiceB = document.getElementById('btn-1');
+let choiceC = document.getElementById('btn-2');
+let choiceD = document.getElementById('btn-3');
+
+let answerCheck = document.getElementById('answer-check');
+
+// Score Section - links to final score, player initials input, and sumbit button
+let scoreSection = document.getElementById('score-section');
+let finalScore = document.getElementById('final-score');
+let playerInitials = document.getElementById('player-initials');
+let playerInitialsBtn = document.getElementById('player-initials-btn');
+
+// High Score Section - links to highscore with a button to go back to start and clear highscore
+let highScoreSection = document.getElementById('high-score-section');
+let listOfHighScores = document.getElementById('list-of-high-scores');
+let goBackBtn = document.getElementById('go-back-btn');
+let clearHighScoreBtn = document.getElementById('clear-high-score-btn');
+
+ // Set the questions of Object
+ const questions = [
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+    {
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "a. //This is a comment"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c. myFunctions()"
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
+    },
+    {
+        question: "How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b. function myFunction()"
+    },
+    
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+    {
+        question: "Who invented JavaScript?",
+        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
+        answer: "c. Brendan Eich"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
+        answer: "c. if(i == 5)"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+];
+
+// Counters
+let correctAnswer = 0;
+let questionIndex = 0;
+
+/* FUNCTION SECTION */
+// click "Start Quiz" to start timer
+let totalTime = 150
+function newQuiz() {
+    questionIndex = 0;
+    totalTime = 150;
+    timeLeft.textContent = totalTime;
+    playerInitials.textConent = '';
+
+    startSection.style.display = 'none';
+    quizSection.style.display = 'block';
+    countdownTimer.style.display = 'block';
+    timesUp.style.display = 'none';
+
+    let startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if (totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
+                gameOver();
+            }
+        }
+    }, 1000);
+    nextQuestion();
+};
+
+// Goes to the next questions of the array
+function nextQuestion() {
+    quizQuestion.textContent = questions[questionIndex].question;
+    choiceA.textContent = questions[questionIndex].choices[0];
+    choiceB.textContent = questions[questionIndex].choices[1];
+    choiceC.textContent = questions[questionIndex].choices[2];
+    choiceD.textContent = questions[questionIndex].choices[3];
+}
+
+// Checks if the answer is right then updates correctAnswer to have a point
+function checkAnswer(answer) {
+    let lineBreak = document.getElementById('line-break');
+    lineBreak.style.display = 'block';
+    answerCheck.style.display = 'block';
+
+    if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+        correctAnswer++;
+        answerCheck.textContent = 'Correct!';
+    } else {
+        totalTime -= 10;
+        timeLeft.textContent = totalTime;
+        answerCheck.textContent = `Wrong! The correct answer is: ${questions[questionIndex].answer}`
+    }
+
+    questionIndex++
+    if (questionIndex < questions.length) {
+        nextQuestion();
+    } else {
+        gameOver();
+    }
+}
+
+function chooseA() {
+    checkAnswer(0);
+}
+function chooseB() {
+    checkAnswer(1);
+}
+function chooseC() {
+    checkAnswer(2);
+}
+function chooseD() { 
+    checkAnswer(3);
+}
+
+// Displays the scoreSection, times up and the score of correctAnswer
+function gameOver() {
+    scoreSection.style.display = 'block';
+    quizSection.style.display = 'none';
+    startSection.style.display = 'none';
+    countdownTimer.style.display = 'none';
+    timesUp.style.display = 'block';
+    finalScore.textContent = correctAnswer;
+}
+
+// Stores players high score in local storage
+function storeHighScores(event) {
+    event.preventDefault();
+    // Prevents player from entering blank value
+    if (playerInitials.value === '') {
+        alert('Please enter your initials!');
+        return;
+    }
+
+    startSection.style.display = 'none';
+    countdownTimer.style.display = 'block';
+    timesUp.style.display = 'none';
+    scoreSection.style.display = 'none';
+    highScoreSection.style.display = 'block';
+
+    // pulls from local storage
+    let savedHighScores = localStorage.getItem('high scores');
+    let scoresArray;
+
+    if (savedHighScores === null) {
+        scoresArray = [];
+    } else {
+        scoresArray = JSON.parse(savedHighScores)
+    }
+
+    let userScore = {
+        initials: playerInitials.value,
+        score: finalScore.textContent
+    };
+
+    scoresArray.push(userScore);
+
+    let scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem('high scores', scoresArrayString);
+
+    showHighScores();
+}
+
+// To show the High Score Section and to pull the stored local high scores
+function showHighScores() {
+    startSection.style.display = 'none';
+    countdownTimer.style.display = 'none';
+    quizSection.style.display = 'none';
+    timesUp.style.display = 'none';
+    scoreSection.style.display = 'none';
+    highScoreSection.style.display = 'block';
+
+    let savedHighScores = localStorage.getItem('high scores');
+
+    if (savedHighScores === null) {
+        return;
+    }
+
+    let storedHighScores = JSON.parse(savedHighScores);
+
+    for (let i = 0; i < storedHighScores.length; i++) {
+        let eachHighScore = document.createElement('p');
+        eachHighScore.textContent = `${storedHighScores[i].initials}: ${storedHighScores[i].score}`;
+        listOfHighScores.appendChild(eachHighScore);
+    }
+}
+
+// Start and 4 choice buttons for quiz to listen for a click
+startBtn.addEventListener('click', newQuiz);
+choiceA.addEventListener('click', chooseA);
+choiceB.addEventListener('click', chooseB);
+choiceC.addEventListener('click', chooseC);
+choiceD.addEventListener('click', chooseD);
+
+// To store players score and initials to local storage
+playerInitialsBtn.addEventListener('click', function(event){ 
+    storeHighScores(event);
+});
+
+// To show the High Score Board
+viewHighScore.addEventListener('click', function(event) { 
+    showHighScores(event);
+});
+
+// Go back button on the high score section to go back home
+goBackBtn.addEventListener('click', function() {
+    startSection.style.display = 'block';
+    highScoreSection.style.display = 'none';
+});
+
+// Clears the high scores in local storage 
+clearHighScoreBtn.addEventListener('click', function(){
+    window.localStorage.removeItem('high scores');
+    listOfHighScores.textContent = 'High Scores Cleared!';
+    listOfHighScores.setAttribute('style', 'font-family: "Poppins", sans-serif; font-style: italic;')
+});
